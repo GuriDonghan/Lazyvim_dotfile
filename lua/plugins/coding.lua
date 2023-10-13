4685201ecda3c201846ad727878b9f7afff1c7ce
donghan.kang <donghan.kang@navercorp.com>
2023-10-13 18:00:55 +0900

    add swen, wtf nvim

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -46,6 +46,47 @@ return {
     config = true,
   },
 
+  --wtf.nvim
+  {
+    "piersolenski/wtf.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    event = "VeryLazy",
+    opts = { language = "korean" },
+    keys = {
+      {
+        "gw",
+        mode = { "n" },
+        function()
+          require("wtf").ai()
+        end,
+        desc = "Debug diagnostic with AI",
+      },
+      {
+        mode = { "n" },
+        "gW",
+        function()
+          require("wtf").search()
+        end,
+        desc = "Search diagnostic with Google",
+      },
+    },
+  },
+
+  -- Python managing Virtual Environments:
+  {
+    "ChristianChiarulli/swenv.nvim",
+    opts = {},
+    keys = {
+      {
+        "<leader>lc",
+        "<cmd>lua require('swenv.api').pick_venv()<cr>",
+        desc = "Conda virtual env",
+      },
+    },
+  },
+
   -- -- lnsert Github marks to lualine for Copilot
   -- {
   --   "nvim-lualine/lualine.nvim",
@@ -77,76 +118,6 @@ return {
   --   end,
   -- },
 
-  -- better text objects
-  -- {
-  --   "echasnovski/mini.ai",
-  --   keys = { { "[f", desc = "Prev function" }, { "]f", desc = "Next function" } },
-  --   opts = function()
-  --     -- add treesitter jumping
-  --     ---@param capture string
-  --     ---@param start boolean
-  --     ---@param down boolean
-  --     local function jump(capture, start, down)
-  --       local rhs = function()
-  --         local parser = vim.treesitter.get_parser()
-  --         if not parser then
-  --           return vim.notify("No treesitter parser for the current buffer", vim.log.levels.ERROR)
-  --         end
-  --
-  --         local query = vim.treesitter.get_query(vim.bo.filetype, "textobjects")
-  --         if not query then
-  --           return vim.notify("No textobjects query for the current buffer", vim.log.levels.ERROR)
-  --         end
-  --
-  --         local cursor = vim.api.nvim_win_get_cursor(0)
-  --
-  --         ---@type {[1]:number, [2]:number}[]
-  --         local locs = {}
-  --         for _, tree in ipairs(parser:trees()) do
-  --           for capture_id, node, _ in query:iter_captures(tree:root(), 0) do
-  --             if query.captures[capture_id] == capture then
-  --               local range = { node:range() } ---@type number[]
-  --               local row = (start and range[1] or range[3]) + 1
-  --               local col = (start and range[2] or range[4]) + 1
-  --               if down and row > cursor[1] or (not down) and row < cursor[1] then
-  --                 table.insert(locs, { row, col })
-  --               end
-  --             end
-  --           end
-  --         end
-  --         return pcall(vim.api.nvim_win_set_cursor, 0, down and locs[1] or locs[#locs])
-  --       end
-  --
-  --       local c = capture:sub(1, 1):lower()
-  --       local lhs = (down and "]" or "[") .. (start and c or c:upper())
-  --       local desc = (down and "Next " or "Prev ") .. (start and "start" or "end") .. " of " .. capture:gsub("%..*", "")
-  --       vim.keymap.set("n", lhs, rhs, { desc = desc })
-  --     end
-  --
-  --     for _, capture in ipairs({ "function.outer", "class.outer" }) do
-  --       for _, start in ipairs({ true, false }) do
-  --         for _, down in ipairs({ true, false }) do
-  --           jump(capture, start, down)
-  --         end
-  --       end
-  --     end
-  --   end,
-  -- },
-
-  -- {
-  --   "danymat/neogen",
-  --   keys = {
-  --     {
-  --       "<leader>cc",
-  --       function()
-  --         require("neogen").generate({})
-  --       end,
-  --       desc = "Neogen Comment",
-  --     },
-  --   },
-  --   opts = { snippet_engine = "luasnip" },
-  -- },
-
   -- {
   --   "smjonas/inc-rename.nvim",
   --   cmd = "IncRename",
@@ -169,80 +140,6 @@ return {
   --     },
   --   },
   --   opts = {},
-  -- },
-
-  -- {
-  --   "echasnovski/mini.bracketed",
-  --   event = "BufReadPost",
-  --   config = function()
-  --     local bracketed = require("mini.bracketed")
-  --
-  --     -- local function put(cmd, regtype)
-  --     --   local body = vim.fn.getreg(vim.v.register)
-  --     --   local type = vim.fn.getregtype(vim.v.register)
-  --     --   ---@diagnostic disable-next-line: param-type-mismatch
-  --     --   vim.fn.setreg(vim.v.register, body, regtype or "l")
-  --     --   bracketed.register_put_region()
-  --     --   vim.cmd(('normal! "%s%s'):format(vim.v.register, cmd:lower()))
-  --     --   ---@diagnostic disable-next-line: param-type-mismatch
-  --     --   vim.fn.setreg(vim.v.register, body, type)
-  --     -- end
-  --     --
-  --     -- for _, cmd in ipairs({ "]p", "[p" }) do
-  --     --   vim.keymap.set("n", cmd, function()
-  --     --     put(cmd)
-  --     --   end)
-  --     -- end
-  --     -- for _, cmd in ipairs({ "]P", "[P" }) do
-  --     --   vim.keymap.set("n", cmd, function()
-  --     --     put(cmd, "c")
-  --     --   end)
-  --     -- end
-  --     --
-  --     -- local put_keys = { "p", "P" }
-  --     -- for _, lhs in ipairs(put_keys) do
-  --     --   vim.keymap.set({ "n", "x" }, lhs, function()
-  --     --     return bracketed.register_put_region(lhs)
-  --     --   end, { expr = true })
-  --     -- end
-  --
-  --     bracketed.setup({
-  --       file = { suffix = "" },
-  --       window = { suffix = "" },
-  --       quickfix = { suffix = "" },
-  --       yank = { suffix = "" },
-  --       treesitter = { suffix = "n" },
-  --     })
-  --   end,
-  -- },
-
-  -- -- better increase/descrease
-  -- {
-  --   "monaqa/dial.nvim",
-  --   -- stylua: ignore
-  --   keys = {
-  --     { "<C-a>", function() return require("dial.map").inc_normal() end, expr = true, desc = "Increment" },
-  --     { "<C-x>", function() return require("dial.map").dec_normal() end, expr = true, desc = "Decrement" },
-  --   },
-  --   config = function()
-  --     local augend = require("dial.augend")
-  --     require("dial.config").augends:register_group({
-  --       default = {
-  --         augend.integer.alias.decimal,
-  --         augend.integer.alias.hex,
-  --         augend.date.alias["%Y/%m/%d"],
-  --         augend.constant.alias.bool,
-  --         augend.semver.alias.semver,
-  --         augend.constant.new({ elements = { "let", "const" } }),
-  --       },
-  --     })
-  --   end,
-  -- },
-  --
-  -- {
-  --   "simrat39/symbols-outline.nvim",
-  --   keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
-  --   config = true,
   -- },
 
   {
