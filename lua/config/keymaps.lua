62204c1d79caf0767e9c519ba3f6fc95296c3596
donghan.kang <donghan.kang@navercorp.com>
2023-10-16 10:30:39 +0900

    wezterm feature

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,11 +9,48 @@
 
 -- util.cowboy()
 
+-- -- Move to window using the movement keys
+-- vim.keymap.set("n", "<left>", "<C-w>h")
+-- vim.keymap.set("n", "<down>", "<C-w>j")
+-- vim.keymap.set("n", "<up>", "<C-w>k")
+-- vim.keymap.set("n", "<right>", "<C-w>l")
+
+---
+-- wezterm window navigation
+local nav = {
+  h = "Left",
+  j = "Down",
+  k = "Up",
+  l = "Right",
+}
+
+local function navigate(dir)
+  return function()
+    local win = vim.api.nvim_get_current_win()
+    vim.cmd.wincmd(dir)
+    local pane = vim.env.WEZTERM_PANE
+    if pane and win == vim.api.nvim_get_current_win() then
+      local pane_dir = nav[dir]
+      vim.system({ "wezterm", "cli", "activate-pane-direction", pane_dir }, { text = true }, function(p)
+        if p.code ~= 0 then
+          vim.notify(
+            "Failed to move to pane " .. pane_dir .. "\n" .. p.stderr,
+            vim.log.levels.ERROR,
+            { title = "Wezterm" }
+          )
+        end
+      end)
+    end
+  end
+end
+
 -- Move to window using the movement keys
-vim.keymap.set("n", "<left>", "<C-w>h")
-vim.keymap.set("n", "<down>", "<C-w>j")
-vim.keymap.set("n", "<up>", "<C-w>k")
-vim.keymap.set("n", "<right>", "<C-w>l")
+for key, dir in pairs(nav) do
+  vim.keymap.set("n", "<" .. dir .. ">", navigate(key))
+  vim.keymap.set("n", "<C-" .. key .. ">", navigate(key))
+end
+
+---
 
 -- -- run lua
 -- vim.keymap.set("n", "<leader>cR", util.runlua, { desc = "Run Lua" })
--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -174,4 +174,12 @@ return {
       },
     },
   },
+
+  -- better diffing
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
+    opts = {},
+    keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "DiffView" } },
+  },
 }
